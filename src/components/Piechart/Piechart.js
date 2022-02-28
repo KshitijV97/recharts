bd52725c7f04fed0dd28e6e267d060220227e81e
KshitijV97 <writetokshitij@gmail.com>
2022-02-28 17:53:13 +0530

    Added Piechart example

--- src/components/Piechart/Piechart.js
+++ src/components/Piechart/Piechart.js
@@ -1,5 +1,36 @@
 import { Container } from '../Containers/Container';
+import {
+	PieChart,
+	Pie,
+	Sector,
+	Cell,
+	ResponsiveContainer,
+	Tooltip,
+} from 'recharts';
+
+const data01 = [
+	{ name: 'Group A', value: 400 },
+	{ name: 'Group B', value: 300 },
+	{ name: 'Group C', value: 300 },
+	{ name: 'Group D', value: 200 },
+];
 
 export const Piechart = () => {
-	return <Container>Piechart</Container>;
+	return (
+		<Container>
+			<ResponsiveContainer width={'99%'} height={500}>
+				<PieChart width='100%' height={400}>
+					<Pie
+						data={data01}
+						dataKey='value'
+						cx='50%'
+						cy='50%'
+						outerRadius={100}
+						fill='#8884d8'
+						label
+					/>
+				</PieChart>
+			</ResponsiveContainer>
+		</Container>
+	);
 };
